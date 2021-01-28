5b7cc62c0cfecc57c4908a9f95aac8ae09c3223e
Ivan Kupchyk <ivan.kupchic@gmail.com>
2021-01-28 23:52:51 +0300

    Changed slides

--- ui/src/shared/components/CommonCarouselMenu.tsx/CommonCarouselMenu.tsx
+++ ui/src/shared/components/CommonCarouselMenu.tsx/CommonCarouselMenu.tsx
@@ -2,77 +2,98 @@ import React from "react"
 
 import Carousel from 'react-bootstrap/Carousel'
 
+import firstSlideImage from './carouselImage/1.png'
+import secondSlideImage from './carouselImage/2.jpg'
+import thirdSlideImage from './carouselImage/3.jpg'
+import fourthSlideImage from './carouselImage/4.png'
+import fivthSlideImage from './carouselImage/5.png'
+
 import './CommonCarouselMenu.scss'
 
-function getButton(props: any, url: string) {
-  if (props.url !== url) {
-    return <button>Подробнее</button>;
+function getButton(props: any, text: string, url?: string,) {
+  if (props.url === url && url) {
+    return '';
   }
-  return '';
+  return <a className="button" href="/">{text}</a>;
 }
+
+type CarouselItems = {
+  slideUrl: string;
+
+  texts: {
+    title: string;
+    subTitle: string;
+    exprasionTextForButton: string;
+    buttonText: string;
+  }
+}[];
+
+const carouselItems: CarouselItems = [{
+  slideUrl: firstSlideImage,
+  texts: {
+    title: 'Волшебные дни продаж Volvo',
+    subTitle: 'Рекордные выгожы до 31 января 2021',
+    exprasionTextForButton: '',
+    buttonText: 'Подробнее'
+  }
+}, {
+  slideUrl: secondSlideImage,
+  texts: {
+    title: 'Лизинг авто для физических лиц',
+    subTitle: 'Ежемесячные платежи стали ещё ниже',
+    exprasionTextForButton: 'individual',
+    buttonText: 'Подробнее'
+  }
+}, {
+  slideUrl: thirdSlideImage,
+  texts: {
+    title: 'Лизинг для юридических лиц',
+    subTitle: 'Премиум сервис на доступных условиях',
+    exprasionTextForButton: 'legalPerson',
+    buttonText: 'Подробнее'
+  }
+}, {
+  slideUrl: fourthSlideImage,
+  texts: {
+    title: 'Актив assistance - услуга помощи при страховом случае',
+    subTitle: '',
+    exprasionTextForButton: '',
+    buttonText: 'Подробнее'
+  }
+}, {
+  slideUrl: fivthSlideImage,
+  texts: {
+    title: 'Меняйте старый автомобиль',
+    subTitle: 'Программа Trade-in для новых и б\\у автомобилей',
+    exprasionTextForButton: '',
+    buttonText: 'Подробнее'
+  }
+}];
 
 const CommonCarouselMenu = (props: {url: string}) => {
   return <Carousel>
-    <Carousel.Item>
-      <div
-        className="img firstSlide d-block w-100"
-        // src={firstSlide}
-        // alt="Волшебные дни продаж Volvo"
-      />
-      <Carousel.Caption>
-        <h3>Волшебные дни продаж Volvo</h3>
-        <p>Рекордные выгожы до 31 января 2021</p>
-        <button>Подробнее</button>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <div
-        className="img secondSlide d-block w-100"
-        // src={secondSlide}
-        // alt="Лизинг авто для физических лиц"
-      />
-      <Carousel.Caption>
-        <h3>Лизинг авто для физических лиц</h3>
-        <p>Ежемесячные платежи стали ещё ниже</p>
-        { getButton(props, 'individual') }
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <div
-        className="img thirdSlide d-block w-100"
-        // src={thirdSlide}
-        // alt="Лизинг для юридических лиц"
-      />
-      <Carousel.Caption>
-        <h3>Лизинг для юридических лиц</h3>
-        <p>Премиум сервис на доступных условиях</p>
-        { getButton(props, 'legalPerson') }
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <div
-        className="img fourthSlide d-block w-100"
-        // src={fourthSlide}
-        // alt="Актив assistance"
-      />
-      <Carousel.Caption>
-        <h3>Актив assistance - услуга помощи при страховом случае</h3>
-        {/* <p>Премиум сервис на доступных условиях</p> */}
-        <button>Подробнее</button>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <div
-        className="img fifthSlide d-block w-100"
-        // src={fifthSlide}
-        // alt="Third slide"
-      />
-      <Carousel.Caption>
-        <h3>Меняйте старый автомобиль</h3>
-        <p>Программа Trade-in для новых и б\у автомобилей</p>
-        <button>Подробнее</button>
-      </Carousel.Caption>
-    </Carousel.Item>
+    {
+      carouselItems.map((ci, i) => {
+        return <Carousel.Item key={i}>
+        <div
+          className="img d-block w-100"
+          style={{ backgroundImage: `url(${ci.slideUrl})` }}
+          // src={secondSlide}
+          // alt="Лизинг авто для физических лиц"
+        />
+        {ci.slideUrl}
+        <Carousel.Caption>
+          <div className="wrapper-caption">
+            <div className="wrapper-caption-content">
+              <span className="wrapper-caption-content-title">{ ci.texts.title }</span>
+              <span className="wrapper-caption-content-subTitle">{ ci.texts.subTitle }</span>
+              { getButton(props, ci.texts.buttonText, ci.texts.exprasionTextForButton) }
+            </div>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+      })
+    }
   </Carousel>
 }
 
