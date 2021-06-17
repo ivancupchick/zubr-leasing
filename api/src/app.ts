bcf686378b80a97d75a2171f0fbce3006614d0f2
Ivan Kupchyk <ivan.kupchic@gmail.com>
2021-06-17 04:38:21 +0300

    Added SSH

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,8 +3,11 @@ import { GetVehicle, VehicleWpToNormalFields, WPPostMetaType, WPPostType } from 
 import express from "express";
 import mysql from "mysql2";
 
+// tslint:disable-next-line: no-var-requires
+const mysqlssh = require('mysql-ssh');
+const fs = require('fs');
+
 import nodemailer from 'nodemailer';
-import cors from 'cors';
 
 const app = express();
 app.listen(() => {
@@ -22,49 +25,62 @@ app.get('/api/', (req, res) => {
 
   const cars: GetVehicle[] = [];
 
-  const connection = mysql.createConnection({
-    host: "localhost",
-    user: "zubrleas_root",
-    database: "zubrleas_wp",
-    password: "6587erdf@",
-  }).promise();
+  mysqlssh.connect({
+    host: '93.125.99.123',
+    user: 'izyby',
+    privateKey: fs.readFileSync('dist/id_rsa'),
+    passphrase: '6587erdf@'
+  },{
+    host: 'localhost',
+    user: 'izyby_zubr_leas',
+    password: '6587erdf@',
+    database: 'izyby_wp'
+  })
+  .then((client: any) => {
+    console.log(1);
+    client.query("SELECT * FROM wp_posts WHERE post_type = 'vehicle'" )
+      .then((d: any) => {
+        console.log(2);
 
-  connection.query("SELECT * FROM wp_posts WHERE post_type = 'vehicle'")
-    .then((d) => {
-      console.log(d);
+        // res.send(d[0]);
+        const result: WPPostType[] = (d[0] as any[]).filter(c => !!c.post_name);
+        cars.push(...result.map(r => ({ id: r.ID, post_title: r.post_title, post_name: r.post_name })));
 
-      // res.send(d[0]);
-      const result: WPPostType[] = (d[0] as any[]).filter(c => !!c.post_name);
-      cars.push(...result.map(r => ({ id: r.ID, post_title: r.post_title, post_name: r.post_name })));
+        return client.query(`SELECT * FROM \`wp_postmeta\` WHERE post_id IN (${cars.map(c => c.id).join(', ')})`)
+      }).then((d: any) => {
+        console.log(3);
+        cars.forEach(car => {
+          const carFields: WPPostMetaType[] = (d[0] as any[]).filter(dd => dd.post_id == car.id);
+          car.thumbnail_id = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.thumbnail_id)?.meta_value || '';
+          car.year = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.year)?.meta_value || '';
+          car.bodyType = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.bodyType)?.meta_value || '';
+          car.engineCapacity = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.engineCapacity)?.meta_value || '';
+          car.mileage = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.mileage)?.meta_value || '';
+          car.petrol = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.petrol)?.meta_value || '';
+          car.price = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.price)?.meta_value || '';
+          car.transmission = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.transmission)?.meta_value || '';
+          car.wheelDrive = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.wheelDrive)?.meta_value || '';
+          car.vehicle_overview = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.vehicle_overview)?.meta_value || '';
+          car.video = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.video)?.meta_value || '';
+        })
 
-      return connection.query(`SELECT * FROM \`wp_postmeta\` WHERE post_id IN (${cars.map(c => c.id).join(', ')})`)
-    }).then((d) => {
-      cars.forEach(car => {
-        const carFields: WPPostMetaType[] = (d[0] as any[]).filter(dd => dd.post_id == car.id);
-        car.thumbnail_id = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.thumbnail_id)?.meta_value || '';
-        car.year = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.year)?.meta_value || '';
-        car.bodyType = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.bodyType)?.meta_value || '';
-        car.engineCapacity = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.engineCapacity)?.meta_value || '';
-        car.mileage = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.mileage)?.meta_value || '';
-        car.petrol = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.petrol)?.meta_value || '';
-        car.price = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.price)?.meta_value || '';
-        car.transmission = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.transmission)?.meta_value || '';
-        car.wheelDrive = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.wheelDrive)?.meta_value || '';
-        car.vehicle_overview = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.vehicle_overview)?.meta_value || '';
-        car.video = carFields.find(cf => cf.meta_key == VehicleWpToNormalFields.video)?.meta_value || '';
-      })
-
-      return connection.query(`SELECT * FROM \`wp_posts\` WHERE post_type = 'attachment' AND post_parent IN (${cars.map(c => c.id).join(', ')})`)
-    }).then((d) => {
-      const attachments: WPPostType[] = d[0] as any;
-      cars.forEach(car => {
-        car.attachments = [ (attachments.find(att => att.ID == Number(car.thumbnail_id || 0))?.guid || '') ];
-        car.attachments.push(...attachments.filter(att => att.post_parent === car.id && att.ID != Number(car.thumbnail_id || 0)).map(att => att.guid));
-      })
-      res.send(cars);
-      connection.end();
-    });
-
+        return client.query(`SELECT * FROM \`wp_posts\` WHERE post_type = 'attachment' AND post_parent IN (${cars.map(c => c.id).join(', ')})`)
+      }).then((d: any) => {
+        console.log(4);
+        const attachments: WPPostType[] = d[0] as any;
+        cars.forEach(car => {
+          car.attachments = [ (attachments.find(att => att.ID == Number(car.thumbnail_id || 0))?.guid || '') ];
+          car.attachments.push(...attachments.filter(att => att.post_parent === car.id && att.ID != Number(car.thumbnail_id || 0)).map(att => att.guid));
+        })
+        res.send(cars);
+        // client.end();
+        mysqlssh.close()
+      });
+  })
+  .catch((err: any) => {
+    console.log(err)
+    mysqlssh.close()
+  })
 
   // res.send('Hello World');
 })
